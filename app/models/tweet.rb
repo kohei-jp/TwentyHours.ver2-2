9c8cfe75b9f142a4e79551e3ebfa462a9658328b
kohei-jp <kohei.mitateda@gmail.com>
2020-02-01 15:49:01 +0900

    base

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,10 @@ class Tweet < ApplicationRecord
   validates:text, length: { maximum: 10 }
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    return Tweet.all unless search
+    Tweet.where('text LIKE(?)', "%#{search}%")
+  end
+
 end
