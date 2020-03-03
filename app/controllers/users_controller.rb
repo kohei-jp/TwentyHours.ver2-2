e7a1b373f8e317fa26c76ca5f96f3c582dd9aefc
kohei-jp <kohei.mitateda@gmail.com>
2020-03-03 23:11:13 +0900

    円グラフ　導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,19 @@ class UsersController < ApplicationController
 
   def show # routingで[:id]が渡ってきている
     @user = User.find(params[:id])
-    @tags = @user.tags #これでいいはず(リレーションが上手くいっていれば)
+    @tags = @user.tags 
     @name = @user.name
     @tweets = current_user.tweets.includes(:user, :tag).order("created_at DESC").page(params[:page]).per(9)
     @times = Tweet.group('user_id, tag_id').sum(:time) #fav用
-    @mytimes = @user.tweets.group_by_day(:created_at).sum(:time)
+    @mytimes = @user.tweets.group_by_day(:created_at).sum(:time).to_a
+    binding.pry
+
+    # @pietimes = @tags.includes(:tweets).group_by(:tag_name).sum(:time) 上手くいかない
+    @pietimes = @user.tweets.includes(:tag).group('tag_name').sum(:time).to_a
 
     tweets = @user.tweets #fav用
     @favorite_tweets = @user.favorite_tweets #fav用
     # favorite_tweetsとは、user.rbで定義した、favしたtweetsテーブルのこと
-    # binding.pry
 
     # user = User.find(params[:id]) #follow用
     @fallow_users = @user.followings #follow用
@@ -26,12 +29,12 @@ class UsersController < ApplicationController
 
   end
 
+  # 円グラフのやつ??
   def calc_total
     tweets.joins(:tags)
       .group(Tweets.arel_table[:id]).select('project_users.*, sum(work_time) as sum_work_times, sum(extra_cost) as sum_extra_costs')
   end
 
-# ==============追加================
 def follows
   user = User.find(params[:id])
   @users = user.followings
@@ -43,6 +46,5 @@ def followers
   @users = user.followers
   redirect_to tweet_path
 end
-# ==============追加================
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -72,9 +72,6 @@ ActiveRecord::Schema.define(version: 2020_02_16_112426) do
     t.string "provider"
     t.string "uid"
     t.string "username"
-    t.string "location"
-    t.string "image"
-    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
