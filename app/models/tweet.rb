5140c89c2c1118b7fbeb0a9f274e182151908bdc
kohei-jp <kohei.mitateda@gmail.com>
2020-03-06 04:09:03 +0900

    軽微な修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -16,4 +16,11 @@ class Tweet < ApplicationRecord
     favorites.where(user_id: user.id).exists?
   end
 
+  # SQLインジェクション対策
+  def self.search(search)
+    return Tweet.all unless search
+    search = "%#{search}%"
+    Tweet.find_by_sql(["select * from tweets where text like ? ", search]) #この1行
+  end
+
 end
