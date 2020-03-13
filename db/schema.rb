9c4ecb4e782153f6068ec69116e6023591816419
kohei-jp <kohei.mitateda@gmail.com>
2020-03-13 17:46:25 +0900

    userテーブルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -69,9 +69,6 @@ ActiveRecord::Schema.define(version: 2020_03_11_032132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "provider"
-    t.string "uid"
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
