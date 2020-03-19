081d6d3957b7eb21b3703a8d8e3148e7c854ffa6
kohei-jp <kohei.mitateda@gmail.com>
2020-03-19 19:45:27 +0900

    インクリメンタルサーチ

--- app/controllers/tweets/searches_controller.rb
+++ app/controllers/tweets/searches_controller.rb
@@ -2,7 +2,9 @@ class Tweets::SearchesController < ApplicationController
   def index
     # binding.pry
     @tweets = Tweet.search(params[:keyword])
+    @times = Tweet.joins(:tag, :user).group('name').group('tag_name').sum(:time)
     respond_to do |format|
+    
       format.html
       format.json
     end
